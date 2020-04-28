c3a77b8adf1047ac6ef177fd24f146ea406c5c85
Daniel Roe <daniel@parentscheme.com>
2020-04-28 22:46:10 +0100

    chore: compile to cjs, es and browser builds using rollup

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+    {
+      file: pkg.browser,
+      format: 'iife',
+      name: 'VueSanity',
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    }),
+  ],
+}
