3c2c56b0191f7a93fdf589ea5a134ec59c04b027
Daniel Roe <daniel@concision.co.uk>
2020-04-30 21:31:04 +0100

    fix: allow rollup compilation

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import { resolve, join } from 'path'
-export { useFetch } from './fetch'
-export { withContext } from './context'
 import { Module } from '@nuxt/types'
-const libRoot = resolve(__dirname, '..')
 
 const compositionApiModule: Module<any> = function () {
+  const libRoot = resolve(__dirname, '..')
   const { dst } = this.addTemplate({
     src: resolve(libRoot, 'lib', 'plugin.js'),
     fileName: join('composition-api', 'plugin.js'),
     options: {},
   })
-  this.options.plugins.push(resolve(this.options.buildDir, dst))
+  this.options.plugins = this.options.plugins || []
+  this.options.plugins.push(resolve(this.options.buildDir || '', dst))
 }
 
 export default compositionApiModule
+// eslint-disable-next-line
+export const meta = require('../package.json')
+
+export { useFetch } from './fetch'
+export { withContext } from './context'
