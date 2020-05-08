274c6181a6c03b0a158fad5ae5cb3ae0c0702f42
Sebastian Krüger <2pi_r2@gmx.de>
2020-05-08 10:10:06 +0200

    add computed test

---
+++ test/e2e/isComputed.ts
@@ -0,0 +1,25 @@
+import { navigateTo, expectOnPage } from './helpers'
+
+// eslint-disable-next-line
+fixture`isComputed`
+
+test('detects not computed', async t => {
+  await navigateTo('/computed')
+  await expectOnPage('test1-no')
+  await expectOnPage('test2-no')
+  await expectOnPage('test3-no')
+  await expectOnPage('test4-no')
+  await expectOnPage('test5-no')
+  await expectOnPage('test6-no')
+})
+
+test('detects computed with no setter', async t => {
+  await navigateTo('/computed')
+  await expectOnPage('test7-yes')
+  await expectOnPage('test8-yes')
+})
+
+test('detects not computed with setter', async t => {
+  await navigateTo('/computed')
+  await expectOnPage('test9-no')
+})
---
+++ test/fixture/pages/computed.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- <div v-for="c in values" :key="c[0]">{{ c[0] }}-{{ isComputed(c[1]) ? 'yes' : 'no'}}</div> -->
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'nuxt-composition-api'
+
+export default defineComponent({
+  setup() {
+    // console.log('test')
+    // const values = [
+    //   ['test1', null],
+    //   ['test2', {}],
+    //   ['test3', ''],
+    //   ['test4', ref(null)],
+    //   ['test5', ref(5)],
+    //   ['test6', ref({})],
+    //   ['test7', computed(() => 5)],
+    //   ['test8', computed(() => null)],
+    //   ['test9', computed({
+    //     get: () => 5,
+    //     set: val => {},
+    //   })]
+    // ].map(v => [v[0], isComputed(v[1]) ? 'yes' : 'no'])
+
+    // console.log(values)
+
+    // return {
+    //   values,
+    //   isComputed,
+    // }
+  },
+})
+</script>
