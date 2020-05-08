ae645f71f0b7eec39edfe92a9689835b952c2785
Daniel Roe <daniel@concision.co.uk>
2020-05-08 16:04:18 +0100

    docs: add minimal vuepress docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  title: '🏗️ Nuxt Composition API',
+  description: 'Composition API hooks for Nuxt',
+  evergreen: true,
+  dest: './dist',
+  themeConfig: {
+    repo: 'nuxt-community/composition-api',
+    editLinks: true,
+    docsDir: 'docs',
+    sidebarDepth: 2,
+    sidebar: {
+      '/': [
+        {
+          title: 'Setup',
+          collapsable: false,
+          children: ['/', '/setup'],
+        },
+        {
+          title: 'Helpers',
+          collapsable: false,
+          children: [
+            '/helpers/ssrRef',
+            '/helpers/useAsync',
+            '/helpers/useContext',
+            '/helpers/useFetch',
+            '/helpers/useHead',
+          ],
+        },
+        {
+          title: 'Examples',
+          collapsable: false,
+          children: ['/examples/useFetch'],
+        },
+      ],
+    },
+  },
+}
