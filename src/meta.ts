f3024e1e79a01a2d9714b4549bd6d6966b2ef260
Daniel Roe <daniel@concision.co.uk>
2020-05-10 13:46:43 +0100

    fix: improve default values for `useMeta`

--- src/meta.ts
+++ src/meta.ts
@@ -1,19 +1,25 @@
 import { MetaInfo } from 'vue-meta'
 
 type MetaInfoMapper<T> = {
-  [P in keyof T]: T[P] extends Array<any> ? T[P] : T[P] | undefined
+  [P in keyof T]: P extends 'base'
+    ? T[P] | undefined
+    : T[P] extends Function
+    ? T[P] | undefined
+    : T[P] extends Array<any> | Record<string, any>
+    ? T[P]
+    : T[P] | undefined
 }
 
 export function createEmptyMeta(): MetaInfoMapper<Required<MetaInfo>> {
   return {
     __dangerouslyDisableSanitizers: [],
-    __dangerouslyDisableSanitizersByTagID: undefined,
+    __dangerouslyDisableSanitizersByTagID: {},
 
     title: undefined,
     titleTemplate: undefined,
-    htmlAttrs: undefined,
-    headAttrs: undefined,
-    bodyAttrs: undefined,
+    htmlAttrs: {},
+    headAttrs: {},
+    bodyAttrs: {},
 
     base: undefined,
 
