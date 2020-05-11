b4c17f23f2d854691361c5319d6dcd41de103ed3
Daniel Roe <daniel@parentscheme.com>
2020-05-11 14:33:22 +0100

    test: add e2e test

--- test/fixture/pages/ssr-ref.vue
+++ test/fixture/pages/ssr-ref.vue
@@ -5,7 +5,7 @@
     <div>prefetched-{{ prefetchValue }}</div>
     <div>on: {{ asyncValue }}</div>
     <div>no-change: {{ noChange }}</div>
-    <div>shallow-{{ shallow.v }}</div>
+    <div>shallow-{{ shallow.v.deep }}</div>
     <nuxt-link to="/">home</nuxt-link>
   </div>
 </template>
@@ -20,6 +20,7 @@ import {
   onServerPrefetch,
   useAsync,
   shallowSsrRef,
+  onMounted,
 } from 'nuxt-composition-api'
 
 export function fetcher(result, time = 100) {
@@ -36,9 +37,12 @@ export default defineComponent({
     const prefetchValue = ssrRef('') // changed => in __NUXT__
     const funcValue = ssrRef(() => 'runs SSR or client-side') // function => in __NUXT__
     const noChange = ssrRef('initValue') // no Change => not in __NUXT__
-    const shallow = shallowSsrRef({ v: 'init' }) // only deep change => not in __NUXT__
 
-    shallow.value.v = 'Hello World'
+    const shallow = shallowSsrRef({ v: { deep: 'init' } })
+    if (process.server) shallow.value = { v: { deep: 'server' } }
+    onMounted(() => {
+      shallow.value.v.deep = 'client'
+    })
 
     const computedVal = computed(() => refValue.value)
 
