9900f1d58d2d69b20a18fda6c10e02a2ff8e5ace
Sebastian Krüger <2pi_r2@gmx.de>
2020-07-18 15:28:40 +0200

    Create createHelpers.ts

---
+++ src/createHelpers.ts
@@ -0,0 +1,6 @@
+import { Middleware, Plugin, Module, ServerMiddleware } from '@nuxt/types'
+
+export const defineNuxtPlugin = (plugin: Plugin) => plugin
+export const defineNuxtMiddleware = (middleware: Middleware) => middleware 
+export const defineNuxtModule = <T={}>(module: Module<T>) => module 
+export const defineNuxtServerMiddleware = (serverMiddleware: ServerMiddleware) => serverMiddleware
