7fbd8fb2795032884dd6f528f0bdcbb8e2d1b9fe
Daniel Roe <daniel@parentscheme.com>
2021-02-11 12:29:20 +0000

    docs: add tsdoc information for new wrapper helpers

--- src/wrappers.ts
+++ src/wrappers.ts
@@ -3,6 +3,11 @@ import type { Store } from 'vuex'
 
 import { getCurrentInstance } from './utils'
 
+/**
+ * You might want to create a custom helper to 'convert' a non-Composition API property to a Composition-ready one. `wrapProperty` enables you to do that easily, returning either a computed or a bare property as required.
+ * @param property the name of the property you would like to access. For example, `$store` to access `this.$store`.
+ * @param makeComputed a boolean indicating whether the helper function should return a computed property or not. Defaults to `true`.
+ */
 export const wrapProperty = <
   K extends keyof NonNullable<ReturnType<typeof getCurrentInstance>>,
   T extends boolean = true
@@ -24,16 +29,57 @@ export const wrapProperty = <
 
 /**
  * Gain access to the router just like using this.$router in a non-Composition API manner.
+ * @example
+  ```ts
+  import { defineComponent, useRouter } from '@nuxtjs/composition-api'
+
+  export default defineComponent({
+    setup() {
+      const router = useRouter()
+      router.push('/')
+    }
+  })
+  ```
  */
 export const useRouter = wrapProperty('$router', false)
 
 /**
- * Gain access to the route just like using this.$route in a non-Composition API manner.
+ * Returns `this.$route`, wrapped in a computed - so accessible from `.value`.
+ * @example
+  ```ts
+  import { computed, defineComponent, useRoute } from '@nuxtjs/composition-api'
+
+  export default defineComponent({
+    setup() {
+      const route = useRoute()
+      const id = computed(() => route.value.params.id)
+    }
+  })
+  ```
  */
 export const useRoute = wrapProperty('$route')
 
 /**
- * Gain access to the store just like using this.$store in a non-Composition API manner.
+ * Gain access to the store just like using this.$store in a non-Composition API manner. You can also provide an injection key or custom type to get back a semi-typed store:
+ * @example
+  ```ts
+  import { defineComponent, useStore } from '@nuxtjs/composition-api'
+
+  export interface State {
+    count: number
+  }
+
+  export const key: InjectionKey<Store<State>> = Symbol()
+
+  export default defineComponent({
+    setup() {
+      const store = useStore()
+      const store = useStore(key)
+      const store = useStore<State>()
+      // In both of these cases, store.state.count will be typed as a number
+    }
+  })
+  ```
  */
 export const useStore = <S>(key?: InjectionKey<S>): Store<S> => {
   const vm = getCurrentInstance()
