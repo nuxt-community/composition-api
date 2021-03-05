65ef93ac4c48288f9b5a216f6151cd08c120fc60
Daniel Roe <daniel@roe.dev>
2021-03-05 12:01:03 +0000

    fix: replace all instances :facepalm:

--- src/vite.ts
+++ src/vite.ts
@@ -17,7 +17,10 @@ export function compositionApiPlugin(): Plugin & { enforce: 'pre' } {
     name: 'nuxt:composition-api',
     enforce: 'pre',
     transform(code: string, filename: string) {
-      code = code.replace('@nuxtjs/composition-api', '~nuxtjs-composition-api')
+      code = code.replace(
+        /@nuxtjs[\\/]composition-api/g,
+        '~nuxtjs-composition-api'
+      )
       const keyedFunctions = /(useStatic|shallowSsrRef|ssrPromise|ssrRef|reqSsrRef|useAsync)/
       if (!keyedFunctions.test(code)) {
         return {
