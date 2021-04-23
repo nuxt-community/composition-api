71e71911ec685d9ce0b1aec800fdeb744658f596
Daniel Roe <daniel@roe.dev>
2021-04-23 11:41:33 +0100

    fix: alias module so we can access `process.server` in vite node build

--- src/runtime/composables/meta.ts
+++ src/runtime/composables/meta.ts
@@ -1,4 +1,4 @@
-import defu from 'defu'
+import { defu } from './utils'
 import {
   computed,
   reactive,
--- src/runtime/composables/utils.ts
+++ src/runtime/composables/utils.ts
@@ -18,3 +18,36 @@ export function getCurrentInstance() {
 
   return vm.proxy as InstanceType<VueConstructor>
 }
+
+function isObject(val: any) {
+  return val !== null && typeof val === 'object'
+}
+
+/** https://github.com/unjs/defu */
+function _defu<T>(baseObj: T, defaults: any): T {
+  if (!isObject(defaults)) {
+    return _defu(baseObj, {})
+  }
+
+  const obj = Object.assign({}, defaults)
+
+  for (const key in baseObj) {
+    if (key === '__proto__' || key === 'constructor') continue
+
+    const val = baseObj[key]
+    if (val === null) continue
+
+    if (Array.isArray(val) && Array.isArray(obj[key])) {
+      obj[key] = obj[key].concat(val)
+    } else if (isObject(val) && isObject(obj[key])) {
+      obj[key] = _defu(val, obj[key])
+    } else {
+      obj[key] = val
+    }
+  }
+
+  return obj
+}
+
+export const defu = (...args: any[]) =>
+  args.reduce((p, c) => _defu(p, c), {} as any)
