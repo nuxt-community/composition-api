283565a1591c969a9d2730178929edc171914330
Daniel Roe <daniel@roe.dev>
2021-09-01 13:29:12 +0100

    fix: preserve reactive objects on `useFetch` hydration

--- test/fixture/pages/ttfb.vue
+++ test/fixture/pages/ttfb.vue
@@ -1,27 +1,29 @@
 <template>
-  <main>TTFB: {{ ttfb }}ms</main>
+  <main>TTFB: {{ dataset.ttfb }}ms</main>
 </template>
 
 <script>
 import {
   defineComponent,
-  ref,
+  reactive,
   useFetch,
   onMounted,
 } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {
+    const dataset = reactive({
+      ttfb: -1
+    })
     useFetch(() => {})
 
-    const ttfb = ref(-1)
     onMounted(() => {
-      ttfb.value =
+      dataset.ttfb =
         globalThis.performance.getEntriesByType('navigation')[0].responseStart
     })
 
     return {
-      ttfb,
+      dataset,
     }
   },
 })
