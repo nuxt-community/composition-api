48e3b700f3a702473c10c844f3e4f3c6a8013889
Rafal Chlodnicki <rchl2k@gmail.com>
2023-03-30 23:49:42 +0200

    disable telemetry in fixture

--- test/fixture/nuxt.config.ts
+++ test/fixture/nuxt.config.ts
@@ -15,6 +15,7 @@ const moduleSource = join(rootDir, inDevelopment ? 'src' : 'dist')
 console.log('Testing', inDevelopment ? 'source' : 'built', 'module')
 
 export default <NuxtConfig>{
+  telemetry: false,
   alias: {
     '@nuxtjs/composition-api/dist/runtime/globals': join(
       moduleSource,
