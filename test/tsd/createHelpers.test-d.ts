347d90da6f7867114b87278438df045dfc38a73d
Daniel Roe <daniel@roe.dev>
2023-03-31 10:26:09 +0100

    test: fix type test

--- test/tsd/createHelpers.test-d.ts
+++ test/tsd/createHelpers.test-d.ts
@@ -7,7 +7,7 @@ import { expectType } from 'tsd'
 defineNuxtPlugin((context, inject) => {
   const hello = (msg: string) => console.log(`Hello ${msg}!`)
 
-  expectType<boolean>(context.isClient)
+  expectType<boolean>(context.isDev)
 
   inject('hello', hello)
 })
