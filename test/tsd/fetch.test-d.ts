9657c55cb34c6ed458528b392240a861b9db918a
Daniel Roe <daniel@roe.dev>
2023-03-31 11:38:02 +0100

    chore: upgrade dependencies (#740

--- test/tsd/fetch.test-d.ts
+++ test/tsd/fetch.test-d.ts
@@ -3,8 +3,8 @@ import { expectType } from 'tsd'
 import { useFetch } from '../..'
 
 expectType<{
-  fetch: () => void
-  $fetch: () => void
+  fetch: () => void | Promise<void>
+  $fetch: () => void | Promise<void>
   fetchState: {
     error: Error | null
     pending: boolean
